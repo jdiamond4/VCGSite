ee4ebdd97d6c5f1bce6da19a121c6cf7d58fff9b
Justin Diamond <justdiamond2006@gmail.com>
2026-08-17 13:36:19 -0400

    Update apply link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { Navbar } from '../components/Navbar'
 import { Hero } from '../components/Hero'
 import { Stats } from '../components/Stats'
@@ -14,19 +15,14 @@ export function Home() {
         <section className="invite" aria-labelledby="invite-heading">
           <div className="invite__inner">
             <div className="invite__content">
-              <h2 id="invite-heading">Join the next cohort.</h2>
+              <h2 id="invite-heading">Join the next cohort</h2>
               <p>
                 Applications open each semester. No consulting experience is necessary. We look for
                 intellectual drive and a willingness to try something new.
               </p>
-              <a
-                className="invite__btn"
-                href="https://forms.gle/LJv3Xh1SJyj2xp3e8"
-                target="_blank"
-                rel="noreferrer"
-              >
+              <Link className="invite__btn" to="/apply">
                 Apply now
-              </a>
+              </Link>
             </div>
           </div>
         </section>
